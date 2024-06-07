4178a104a35429c2a7c88a514f9c7258e1b463a9
Emanikor <87859876+emanikor@users.noreply.github.com>
2024-06-08 02:38:38 +0300

    Update carRentalServiceTest.java

--- carRentalServiceTest.java
+++ carRentalServiceTest.java
@@ -1,158 +1,47 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+package com.mycompany.ASSproject1;
 
-class Car {
-    private String licensePlate;
-    private String model;
-    private boolean isRented;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    public Car(String licensePlate, String model) {
-        this.licensePlate = licensePlate;
-        this.model = model;
-        this.isRented = false;
-    }
+public class ASSproject1{
 
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public boolean isRented() {
-        return isRented;
-    }
-
-    public void rent() {
-        this.isRented = true;
-    }
-
-    public void returnCar() {
-        this.isRented = false;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "licensePlate='" + licensePlate + '\'' +
-                ", model='" + model + '\'' +
-                ", isRented=" + isRented +
-                '}';
-    }
-}
-
-class Customer {
-    private String name;
-    private String driverLicenseNumber;
-
-    public Customer(String name, String driverLicenseNumber) {
-        this.name = name;
-        this.driverLicenseNumber = driverLicenseNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDriverLicenseNumber() {
-        return driverLicenseNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "name='" + name + '\'' +
-                ", driverLicenseNumber='" + driverLicenseNumber + '\'' +
-                '}';
-    }
-}
-
-class RentalAgency {
-    private List<Car> cars;
-    private List<Customer> customers;
-
-    public RentalAgency() {
-        this.cars = new ArrayList<>();
-        this.customers = new ArrayList<>();
-    }
-
-    public void addCar(Car car) {
-        cars.add(car);
-    }
-
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-
-    public Car findAvailableCar(String model) {
-        for (Car car : cars) {
-            if (car.getModel().equals(model) && !car.isRented()) {
-                return car;
-            }
-        }
-        return null;
-    }
-
-    public boolean rentCar(Customer customer, String model) {
-        Car car = findAvailableCar(model);
-        if (car != null) {
-            car.rent();
-            System.out.println("Car rented: " + car);
-            return true;
-        } else {
-            System.out.println("No available car of model: " + model);
-            return false;
-        }
-    }
-
-    public void returnCar(Car car) {
-        car.returnCar();
-        System.out.println("Car returned: " + car);
-    }
-
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    public List<Customer> getCustomers() {
-        return customers;
-    }
-
-    @Override
-    public String toString() {
-        return "RentalAgency{" +
-                "cars=" + cars +
-                ", customers=" + customers +
-                '}';
-    }
-}
-
-public class CarRentalSystem {
-    public static void main(String[] args) {
+    @Test
+    public void testAddCar() {
         RentalAgency rentalAgency = new RentalAgency();
+        Car car = new Car("ABC123", "Toyota Camry");
+        rentalAgency.addCar(car);
+        assertEquals(1, rentalAgency.getCars().size());
+        assertEquals("Toyota Camry", rentalAgency.getCars().get(0).getModel());
+    }
 
-        // Adding cars to the rental agency
-        rentalAgency.addCar(new Car("ABC123", "Toyota Camry"));
-        rentalAgency.addCar(new Car("XYZ789", "Honda Civic"));
-        rentalAgency.addCar(new Car("LMN456", "Ford Mustang"));
+    @Test
+    public void testAddCustomer() {
+        RentalAgency rentalAgency = new RentalAgency();
+        Customer customer = new Customer("conny emanikor", "D1234567");
+        rentalAgency.addCustomer(customer);
+        assertEquals(1, rentalAgency.getCustomers().size());
+        assertEquals("conny emanikor", rentalAgency.getCustomers().get(0).getName());
+    }
 
-        // Adding customers to the rental agency
-        rentalAgency.addCustomer(new Customer("John Doe", "D1234567"));
-        rentalAgency.addCustomer(new Customer("Jane Smith", "D7654321"));
+    @Test
+    public void testRentCar() {
+        RentalAgency rentalAgency = new RentalAgency();
+        Car car = new Car("ABC123", "Toyota Camry");
+        rentalAgency.addCar(car);
+        Customer customer = new Customer("conny emanikor", "D1234567");
+        rentalAgency.addCustomer(customer);
+        boolean result = rentalAgency.rentCar(customer.getId(), car.getId());
+        assertTrue(result);
+        assertTrue(car.isRented());
+    }
 
-        // Renting a car
-        Customer customer1 = rentalAgency.getCustomers().get(0);
-        rentalAgency.rentCar(customer1, "Toyota Camry");
-
-        // Returning a car
-        Car rentedCar = rentalAgency.getCars().get(0);
-        rentalAgency.returnCar(rentedCar);
-
-        // Print rental agency state
-        System.out.println(rentalAgency);
+    @Test
+    public void testReturnCar() {
+        RentalAgency rentalAgency = new RentalAgency();
+        Car car = new Car("ABC123", "Toyota Camry");
+        rentalAgency.addCar(car);
+        car.setRented(true); // Assuming there is a method to set the car as rented
+        rentalAgency.returnCar(car.getId()); // Assuming there is a method to return the car by ID
+        assertFalse(car.isRented());
     }
 }
-
